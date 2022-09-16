8d37ee337eebda359159d1e738ac0c44b0875113
Gerardo Garcia <1ggarcia101@gmail.com>
2022-09-16 10:56:43 -0700

    initial code password generator

---
+++ script.js
@@ -0,0 +1,37 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//copy paste code jquery turn into javascript
+var passwordBtnEl = $('#password-btn');
+var passwordDisplayEl = $('#password-display');
+
+// Returns a random character that includes alphanumeric and special character values
+function getPasswordCharacter() {
+  return String.fromCharCode(Math.floor(Math.random() * 77) + 34);
+}
+
+// Returns a string of concatenated characters of length num
+function passwordGenerator(num) {
+  var password = '';
+  for (var i = 0; i < num; i++) {
+    password += getPasswordCharacter();
+  }
+  return password;
+}
+
+passwordBtnEl.on('click', function () {
+  var newPassword = passwordGenerator(15);
+  passwordDisplayEl.text(newPassword);
+});
